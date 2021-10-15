ff54328d66b92212472e80cbcb7e7c6fe1f61340
Tijauna <daniel.ding@mail.utoronto.ca>
2021-10-14 22:35:48 -0400

    initial data loading

--- data_test.py
+++ data_test.py
@@ -1,4 +1,24 @@
-print("this is a test")
+from astropy.io import fits
 
-print(2+3)
-print("testing again")
+
+# Testing
+
+print("Testing allstar")
+with fits.open("allStar-r12-l33.fits") as allStar:
+    # allStar.info()
+
+    print(allStar[0].header[0])
+
+    data = allStar[1].data
+    print(data.shape)
+
+# Testing
+print("Testing contspec")
+
+with fits.open("contspec_dr16_final.fits") as contspec:
+    # allStar.info()
+
+    print(contspec[0].header[0])
+
+    data = contspec[0].data
+    print(data.shape)
